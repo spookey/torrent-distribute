44daa475ad08326fae87347f59652d0d1bf19693
Frieder Grießhammer <frieder.griesshammer@der-beweis.de>
2014-08-23 08:52:10 +0200

    more verbose errormessages

--- helpers.py
+++ helpers.py
@@ -9,27 +9,27 @@ def write_file(filename, content):
     with open(filename, 'w') as f:
         try:
             f.write(content)
-        except Exception:
-            print('Warning: Could not write file %s' %(filename))
+        except Exception as ex:
+            print('warning: could not write file %s: %s' %(filename, ex))
 
 def write_json(filename, content):
     try:
         write_file(filename, dumps(content, indent=2))
-    except Exception:
-        print('Warning: Could not write json file %s' %(filename))
+    except Exception as ex:
+        print('warning: could not write json file %: %s' %(filename, ex))
 
 def read_file(filename):
     if path.exists(filename):
         with open(filename, 'r') as f:
             try:
                 return f.read()
-            except Exception:
-                print('Warning: Could not read file %s' %(filename))
+            except Exception as ex:
+                print('warning: could not read file %s: %s' %(filename, ex))
     else:
         print('Error: file %s does not exist' %(filename))
 
 def read_json(filename):
     try:
         return loads(read_file(filename))
-    except Exception:
-        print('Warning: Could not read json file %s' %(filename))
+    except Exception as ex:
+        print('warning: could not read json file %s: %s' %(filename, ex))
