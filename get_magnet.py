e3582ac9fc380ed43b843bf4713f3262f38aff50
Frieder Grießhammer <frieder.griesshammer@der-beweis.de>
2014-08-24 16:53:30 +0200

    Generating some html output for torrents..

--- get_magnet.py
+++ get_magnet.py
@@ -6,7 +6,7 @@ from argparse import ArgumentParser
 from bencode import bencode, bdecode
 from hashlib import sha1
 from base64 import b32encode
-from helpers import read_file, write_json
+from helpers import read_file, write_file, write_json
 
 def get_magnet(torrentfile):
     metadata = bdecode(read_file(torrentfile))
@@ -14,10 +14,19 @@ def get_magnet(torrentfile):
     digest = sha1(hashcontents).digest()
     return 'magnet:?xt=urn:btih:%s' %(b32encode(digest))
 
+def get_html(mdict):
+    result = '<p>\n\t<h2>Magnet Links</h2>\n\n\t<ul>\n'
+    for magnet in mdict:
+        result += '\t\t<li>\n\t\t\t<a href="%s">%s</a>\n\t\t</li>\n' %(mdict[magnet], magnet)
+        print(magnet)
+        print(mdict[magnet])
+    result += '\t</ul>\n</p>\n'
+    return result
+
 def parser():
-    parser = ArgumentParser(prog='get-magnet', description='get torrent magnet links for files/folders ~ CLI', epilog='That\'s what she said', add_help=True)
-    parser.add_argument('input', action='store', help='specify input files/folders')
-    parser.add_argument('output', action='store', nargs='?', help='specify output file')
+    parser = ArgumentParser(prog='get-magnet', description='get magnet links from existing torrents ~ CLI', epilog='magneto at your service', add_help=True)
+    parser.add_argument('input', action='store', help='specify torrents for input')
+    parser.add_argument('output', action='store', nargs='?', help='specify output file name')
     return parser.parse_args()
 
 def main():
@@ -27,8 +36,7 @@ def main():
     magnets = dict()
 
     source = args.input
-
-    if source.endswith('/') or '.':
+    if source.endswith('/') or source.endswith('.'):
         for tfile in listdir(source):
             if tfile.endswith('.torrent'):
                 torrentfiles.append(tfile)
@@ -43,10 +51,12 @@ def main():
         for torrent in magnets:
             print('%s\t%s' %(torrent, magnets[torrent]))
     else:
-        if not output.endswith('.json'):
-            output += '.json'
-        write_json(output, magnets)
-
+        if output.endswith('.json'):
+            write_json(output, magnets)
+        elif output.endswith('.html'):
+            write_file(output, get_html(magnets))
+        else:
+            print('error: output was not correct. use file.{json,html} as parameter')
 
 if __name__ == '__main__':
     main()
